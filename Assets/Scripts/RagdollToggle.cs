0f1c33f6c3dc377f9632f420541c4ac362f55be5
00wz <130168402+00wz@users.noreply.github.com>
2024-05-09 15:51:36 +0300

    add ragdoll

---
+++ Assets/Scripts/RagdollToggle.cs
@@ -0,0 +1,68 @@
+
+using UnityEngine;
+
+public class RagdollToggle : MonoBehaviour
+{
+    [SerializeField]
+    private Collider myCollider;
+    [SerializeField]
+    private MonoBehaviour[] myScripts;
+    [SerializeField]
+    private Rigidbody myRigidbody;
+    [SerializeField]
+    private GameObject RagdollRoot;
+    [SerializeField]
+    private Animator Animator;
+    [SerializeField]
+    private bool StartStateIsRagdoll;
+
+    private Rigidbody[] rigidbodies;
+    private Collider[] raddollsColliders;
+
+    void Start()
+    {
+        rigidbodies =RagdollRoot.GetComponentsInChildren<Rigidbody>();
+        raddollsColliders = RagdollRoot.GetComponentsInChildren<Collider>();
+        ToggleRagdoll(StartStateIsRagdoll) ;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            ToggleRagdoll(true);
+        }
+    }
+
+    private void ToggleRagdoll(bool isActive)
+    {
+        myCollider.enabled = !isActive;
+        Vector3 velocity = myRigidbody.velocity;
+        myRigidbody.isKinematic = isActive;
+        Animator.enabled = !isActive;
+        for (int i = 0; i < myScripts.Length; i++)
+        {
+            myScripts[i].enabled = !isActive;
+        }
+        for (int i = 0; i < rigidbodies.Length; i++)
+        {
+            rigidbodies[i].isKinematic = !isActive;
+        }
+        if(isActive)
+        {
+            for (int i = 0; i < rigidbodies.Length; i++)
+            {
+                rigidbodies[i].velocity = velocity;
+            }
+        }
+        for (int i = 0; i < raddollsColliders.Length; i++)
+        {
+            raddollsColliders[i].enabled = isActive;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ToggleRagdoll(true);
+    }
+}
